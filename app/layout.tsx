a83f5da5e66b37775138fdbb2fec60f53de64a8f
rotem <nevgauker@gmail.com>
2024-11-10 15:30:38 +0100

    save all

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import { Footer } from 'react-day-picker'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
