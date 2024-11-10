129d4808a4b1de9b9843308092c73034a2e2322f
rotem <nevgauker@gmail.com>
2024-11-10 23:30:59 +0100

    some code cleanup

--- actions/orders.tsx
+++ actions/orders.tsx
@@ -56,15 +56,7 @@ export async function emailOrderHistory(
 
   const orders = user.orders.map(async order => {
     return {
-      ...order,
-      // downloadVerificationId: (
-      //   await db.downloadVerification.create({
-      //     data: {
-      //       expiresAt: new Date(Date.now() + 24 * 1000 * 60 * 60),
-      //       productId: order.product.id,
-      //     },
-      //   })
-      // ).id,
+      ...order
     }
   })
 
