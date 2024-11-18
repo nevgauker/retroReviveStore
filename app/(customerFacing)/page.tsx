ee387cd6cb21f72a9f49b2b7d1d24e3947f6d57d
rotem <nevgauker@gmail.com>
2024-11-18 15:24:38 +0100

    fixed the main page data fetching

--- app/(customerFacing)/page.tsx
+++ app/(customerFacing)/page.tsx
@@ -5,10 +5,33 @@ import { Button } from '@/components/ui/button';
 import { ProductCard, ProductCardSkeleton } from '@/components/ProductCard';
 import { Suspense } from 'react';
 import ClientPopup from '@/components/client_popup';
+import { cache } from "@/lib/cache"
+import db from '@/db/db';
+import { Product } from '@prisma/client';
 
 export default function HomePage() {
   const cookieStore = cookies();
   const popupShown = cookieStore.get('popupShown');
+
+  const getMostPopularProducts = cache(
+    () => {
+      return db.product.findMany({
+        where: { isAvailableForPurchase: true },
+        orderBy: { orders: { _count: "desc" } },
+        take: 6,
+      })
+    },
+    ["/", "getMostPopularProducts"],
+    { revalidate: 60 * 60 * 24 }
+  )
+
+  const getNewestProducts = cache(() => {
+    return db.product.findMany({
+      where: { isAvailableForPurchase: true },
+      orderBy: { createdAt: "desc" },
+      take: 6,
+    })
+  }, ["/", "getNewestProducts"])
 
   return (
     <main className='space-y-12'>
@@ -16,16 +39,24 @@ export default function HomePage() {
       <ProductGridSection
         title='Most Popular'
         bannerImageName='/most.jpg'
+        productsFetcher={getMostPopularProducts}
       />
       <ProductGridSection
         title='Newest'
         bannerImageName='/new.jpg'
+        productsFetcher={getNewestProducts}
       />
     </main>
   );
 }
 
-function ProductGridSection({ title, bannerImageName }: { title: string, bannerImageName: string }) {
+type ProductGridSectionProps = {
+  title: string
+  bannerImageName: string
+  productsFetcher: () => Promise<Product[]>
+}
+
+function ProductGridSection({ title, bannerImageName, productsFetcher }: ProductGridSectionProps) {
   return (
     <div className='space-y-4'>
       <div className='relative w-full h-80'>
@@ -40,10 +71,28 @@ function ProductGridSection({ title, bannerImageName }: { title: string, bannerI
         </Button>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        <Suspense fallback={<ProductCardSkeleton />}>
-          {/* Assume ProductSuspense fetches and displays products */}
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductSuspense productsFetcher={productsFetcher} />
         </Suspense>
       </div>
     </div>
   );
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>
+}) {
+  return (await productsFetcher()).map(product => (
+    <ProductCard key={product.id} {...product} />
+  ))
 }
